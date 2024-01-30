662590980063a009119f20c3396bf59803944bcc
webmasterscity <ds000082@gmail.com>
2024-01-30 12:57:42 -0400

    creacion base sdk

---
+++ src/EpaycoSdkRedeban.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Epayco\SdkRedeban;
+
+class EpaycoSdkRedeban
+{
+    public function __construct()
+    {
+        // Puedes realizar cualquier inicialización aquí
+    }
+
+    public function helloWorld()
+    {
+        return "¡Hola, mundo!";
+    }
+
+    // Puedes agregar más métodos y funcionalidades aquí
+}
