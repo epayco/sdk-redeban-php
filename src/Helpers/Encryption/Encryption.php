46a7e001934e23aced8068383d4005b827f05000
Rogert Castillo <rogert.castillo@epayco.com>
2024-03-22 12:15:45 -0400

    (feat): Remove Encryption

--- src/Helpers/Encryption/Encryption.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Helpers\Encryption;
-
-interface Encryption
-{
-    public function encrypt(string $data, array $options);
-    public function decrypt(string $data, array $options);
-}
