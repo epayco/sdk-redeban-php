493ff9550d29e741c88fec6279b22e73eefb1c06
Rogert Castillo <rogert.castillo@epayco.com>
2024-05-21 11:33:44 -0400

    feature: Adding reverse functionality.

--- src/Services/ReverseService.php
+++ src/Services/ReverseService.php
@@ -9,15 +9,18 @@ class ReverseService extends Service
 {
     protected const MAX_RETRY_REVERSE_REDEBAN = 3;
     public mixed $outData;
-    public function __invoke(ShopDto $inputData): bool
+    public function __invoke($inputData): bool
     {
         $restFinalPos = [];
+        $obj = json_decode(json_encode($inputData));
+        $purchaseService = new ShopService();
+        $reverseRequest = $purchaseService->generateRequestShop($obj);
         $retry = 1;
         try {
             $redebanRepository = new RedebanRepository();
             do {
-                $redebanResponse = $redebanRepository->reverse($inputData);
-                $restFinalPos = $redebanResponse;
+                $redebanResponse = $redebanRepository->reverse($reverseRequest);
+                $restFinalPos = (array)$redebanResponse;
                 $status = isset($redebanResponse->infoRespuesta->codRespuesta) && $redebanResponse->infoRespuesta->codRespuesta == '00';
                 $retry++;
             } while (!$status && $retry < self::MAX_RETRY_REVERSE_REDEBAN);
@@ -29,6 +32,7 @@ class ReverseService extends Service
         $restFinalPos['log_request']    = $inputData;
         $restFinalPos['log_response']   = $redebanResponse ?? null;
         $this->outData = $restFinalPos;
+
 
         return $status;
         
