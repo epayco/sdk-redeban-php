f6a8ed3bdbdf4a14cd6e498787f263fc0eca333c
rhaenyra <leomar.esparragoza@payco.co>
2024-09-16 17:58:22 -0400

    fix: guardar toda la info en test

--- src/Helpers/UtilsPresentSales.php
+++ src/Helpers/UtilsPresentSales.php
@@ -1,14 +1,19 @@
 <?php
 
 namespace Epayco\SdkRedeban\Helpers;
+use Epayco\SdkRedeban\Helpers\PurchaseConfig;
 
 trait UtilsPresentSales
 {
     public function removeCardData($request): mixed
     {
-        unset($request->infoMedioPago->infoEMV);
-        unset($request->infoMedioPago->idTrack);
-        unset($request->datosAdicionales);
+        $sdkConfig = PurchaseConfig::getInstance();
+        $ePurchaseConfig = $sdkConfig->getConfig();
+        if ($ePurchaseConfig->environment !== 'test') {
+            unset($request->infoMedioPago->infoEMV);
+            unset($request->infoMedioPago->idTrack);
+            unset($request->datosAdicionales);
+        }
         return $request;
     }
 }
