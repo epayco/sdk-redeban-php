8e0677cdf7586d2d03801cf05874fd8ef35d1062
rhaenyra <leomar.esparragoza@payco.co>
2024-09-16 17:58:22 -0400

    fix: guardar toda la info en test

--- src/Helpers/UtilsPresentSales.php
+++ src/Helpers/UtilsPresentSales.php
@@ -1,14 +1,18 @@
 <?php
 
 namespace Epayco\SdkRedeban\Helpers;
+use Epayco\SdkRedeban\DTOs\Apisac\DataConfigSdkDto;
 
 trait UtilsPresentSales
 {
     public function removeCardData($request): mixed
     {
-        unset($request->infoMedioPago->infoEMV);
-        unset($request->infoMedioPago->idTrack);
-        unset($request->datosAdicionales);
+        $this->sdkConfig = new DataConfigSdkDto();
+        if ($this->sdkConfig->environment !== 'test') {
+            unset($request->infoMedioPago->infoEMV);
+            unset($request->infoMedioPago->idTrack);
+            unset($request->datosAdicionales);
+        }
         return $request;
     }
 }
